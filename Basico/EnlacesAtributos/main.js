2eba007960c5094ec439d8c69e0f8221a84982f0
FelixZamora <109540098+FelixZamora@users.noreply.github.com>
2022-08-27 13:28:21 -0500

    Clase

---
+++ Basico/EnlacesAtributos/main.js
@@ -0,0 +1,9 @@
+'use strict';
+// Los contenedores que posee JS
+
+var variable1 = 3; // Ambito global
+let variable2 = 4; // Ambito local
+const variable3 = 1;
+
+console.log(variable1+variable2);
+//alert("El valor de la variable: " + variable2);
